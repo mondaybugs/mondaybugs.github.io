73b6783dd218f59f256dc3a4fb0cc85708b74da5
mondaybugs <98820101+mondaybugs@users.noreply.github.com>
2022-02-01 07:49:50 -0500

    Initial commit

---
+++ index.bundle.js
@@ -0,0 +1,252 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["minutetimerpackage"] = factory();
+	else
+		root["MT"] = factory();
+})((typeof self!='undefined'?self:this), function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./appconfig.json":
+/*!**********************************************************************************!*\
+  !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./appconfig.json ***!
+  \**********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "appconfig.json");
+
+/***/ }),
+
+/***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./index.html":
+/*!******************************************************************************!*\
+  !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./index.html ***!
+  \******************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "index.html");
+
+/***/ }),
+
+/***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./shatter.mp3":
+/*!*******************************************************************************!*\
+  !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./shatter.mp3 ***!
+  \*******************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "shatter.mp3");
+
+/***/ }),
+
+/***/ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./timer.css":
+/*!*****************************************************************************!*\
+  !*** ../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./timer.css ***!
+  \*****************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "timer.css");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "reset": () => (/* binding */ reset)
+/* harmony export */ });
+// @ts-nocheck
+A1lib.identifyApp('appconfig.json');
+__webpack_require__(/*! !file-loader?name=[name].[ext]!./index.html */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./index.html");
+__webpack_require__(/*! !file-loader?name=[name].[ext]!./appconfig.json */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./appconfig.json");
+__webpack_require__(/*! !file-loader?name=[name].[ext]!./shatter.mp3 */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./shatter.mp3");
+__webpack_require__(/*! !file-loader?name=[name].[ext]!./timer.css */ "../node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./timer.css");
+const alertSound = new Audio('./shatter.mp3');
+const chat = new Chatbox.default();
+const timerInterval = 1000;
+const clock = document.getElementById('clock');
+const minutes = document.getElementById('minutes');
+const remainder = document.getElementById('remainder');
+const remainderBar = document.getElementById('shrinkBar');
+const tooltipCheckbox = document.getElementById('tooltip');
+const audioCheckbox = document.getElementById('audio');
+alertSound.volume = 0.2;
+let startTime;
+let currentTime;
+let targetTime;
+const readChatbox = () => {
+    if (currentTime >= targetTime)
+        reset();
+    clock.innerText = `${currentTime.getHours().toString().padStart(2, '0')}:${currentTime.getMinutes().toString().padStart(2, '0')}:${currentTime.getSeconds().toString().padStart(2, '0')}`;
+    const remaining = new Date(targetTime.getTime() - currentTime.getTime());
+    remainder.innerText = `${remaining.getUTCHours().toString().padStart(2, '0')}:${remaining.getMinutes().toString().padStart(2, '0')}:${remaining.getSeconds().toString().padStart(2, '0')}`;
+    remainderBar.style.width = `${100 * (currentTime.getTime() - targetTime.getTime()) / (startTime.getTime() - targetTime.getTime())}%`;
+    const opts = chat.read();
+    let chats = '';
+    for (let a in opts)
+        chats += opts[a].text + ' ';
+    if (!chats.length)
+        return currentTime.setTime(currentTime.getTime() + timerInterval);
+    const latestTimestamp = chat.lastTimestampUpdate;
+    currentTime.setTime(latestTimestamp);
+};
+const findChat = setInterval(() => {
+    if (chat.pos === null)
+        return chat.find();
+    clearInterval(findChat);
+    chat.pos.mainbox = chat.pos.boxes[0];
+    showSelectedChat(chat.pos);
+    currentTime = new Date();
+    reset();
+    setInterval(() => {
+        readChatbox();
+    }, timerInterval);
+}, timerInterval);
+const showSelectedChat = (pos) => {
+    try {
+        window.alt1.overLayRect(A1lib.mixColor(0, 255, 0), pos.mainbox.rect.x, pos.mainbox.rect.y, pos.mainbox.rect.width, pos.mainbox.rect.height, 2000, 5);
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+let counter = 0;
+let tooltipHandler;
+const closeTooltip = () => {
+    if (++counter > 9 && window.alt1.rsLastActive < 1000) {
+        window.alt1.clearTooltip();
+        clearInterval(tooltipHandler);
+    }
+};
+const reset = () => {
+    if (audioCheckbox.checked)
+        alertSound.play();
+    if (tooltipCheckbox.checked) {
+        window.alt1.setTooltip("Server Minute");
+        counter = 0;
+        tooltipHandler = setInterval(closeTooltip, 100);
+    }
+    startTime = new Date(currentTime);
+    targetTime = new Date(currentTime);
+    targetTime.setTime(targetTime.getTime() + 60000 * minutes.value);
+    targetTime.setSeconds(0);
+};
+
+
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
