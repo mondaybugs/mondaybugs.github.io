26408ed7b9305e995a6bb5ff08da5fb7c5af2049
mondaybugs <j.ason1231994@gmail.com>
2022-11-30 14:03:44 -0500

    Improve diff visibility

--- ms-spot-the-difference/out.js
+++ ms-spot-the-difference/out.js
@@ -1,10 +1,15 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const c = document.getElementById('spotdiff');
+
+let cycle;
+
+let cnum = 0;
 
 const colour_match = (a, b) => ((Math.abs(a[0] - b[0]) < 4) && (Math.abs(a[1] - b[1]) < 4) && (Math.abs(a[2] - b[2]) < 4));
 
 const spot_diff = async (blob) => {
   while (c.firstChild) c.removeChild(c.firstChild);
+
+  clearInterval(cycle);
 
   const img = document.getElementById('preview');
 
@@ -90,28 +95,78 @@ const spot_diff = async (blob) => {
   right_canvas.getContext("2d").putImageData(right_img_data, 0, 0);
 
   c.appendChild(left_canvas);
-  c.appendChild(right_canvas);
 
   const left_ctx = left_canvas.getContext("2d", { willReadFrequently: true });
   const right_ctx = right_canvas.getContext("2d", { willReadFrequently: true });
+
+  const diff_canvas_b = document.createElement("canvas");
+  diff_canvas_b.width = diff_width;
+  diff_canvas_b.height = diff_height;
 
   const diff_canvas = document.createElement("canvas");
   diff_canvas.width = diff_width;
   diff_canvas.height = diff_height;
 
+  const diff_canvas2 = document.createElement("canvas");
+  diff_canvas2.width = diff_width;
+  diff_canvas2.height = diff_height;
+
+  const diff_b_ctx = diff_canvas_b.getContext("2d");
   const diff_ctx = diff_canvas.getContext("2d");
-  diff_ctx.fillStyle = "black";
-  diff_ctx.fillRect(0, 0, diff_canvas.width, diff_canvas.height);
+  const diff_ctx2 = diff_canvas2.getContext("2d");
+
+  diff_ctx.putImageData(left_ctx.getImageData(0, 0, diff_width, diff_height), 0, 0);
+  diff_ctx2.putImageData(left_ctx.getImageData(0, 0, diff_width, diff_height), 0, 0);
+
+  diff_b_ctx.fillStyle = 'black';
+  diff_b_ctx.fillRect(0, 0, diff_width, diff_height);
 
   for (let y = 0; y < diff_height; y++) {
     for (let x = 0; x < diff_width; x++) {
       const left_px = left_ctx.getImageData(x, y, 1, 1);
       const right_px = right_ctx.getImageData(x, y, 1, 1);
-      if (!colour_match(left_px.data, right_px.data)) diff_ctx.putImageData(left_px, x, y);
+
+      if (!colour_match(left_px.data, right_px.data)) {
+        const r = new ImageData(new Uint8ClampedArray([255, 0, 0, 255]), left_px.width, left_px.height);
+        const g = new ImageData(new Uint8ClampedArray([0, 255, 0, 255]), left_px.width, left_px.height);
+        const b = new ImageData(new Uint8ClampedArray([0, 0, 255, 255]), left_px.width, left_px.height);
+        
+        left_ctx.putImageData(r, x, y);
+        diff_ctx.putImageData(g, x, y);
+        diff_ctx2.putImageData(b, x, y);
+        diff_b_ctx.putImageData(right_px, x, y);
+      }
     }
   }
 
+  left_canvas.style.display = 'inline';
+  diff_canvas.style.display = 'none';
+  diff_canvas2.style.display = 'none';
+
   c.appendChild(diff_canvas);
+  c.appendChild(diff_canvas2);
+  c.appendChild(diff_canvas_b);
+
+  cycle = setInterval(() => {
+    cnum = (cnum + 1) % 3;
+    switch (cnum) {
+      case 0:
+        left_canvas.style.display = 'inline';
+        diff_canvas.style.display = 'none';
+        diff_canvas2.style.display = 'none';
+        break;
+      case 1:
+        left_canvas.style.display = 'none';
+        diff_canvas.style.display = 'inline';
+        diff_canvas2.style.display = 'none';
+        break;
+      case 2:
+        left_canvas.style.display = 'none';
+        diff_canvas.style.display = 'none';
+        diff_canvas2.style.display = 'inline';
+        break;
+    }
+  }, 1000);
 };
 
 document.onpaste = (event) => {
@@ -123,5 +178,3 @@ document.onpaste = (event) => {
     }
   }
 };
-
-},{}]},{},[1]);
