3c04d01b15faca2713ed36b4efb0688105ea9024
mondaybugs <j.ason1231994@gmail.com>
2022-12-02 04:04:46 -0500

    Optimize and clean up Resolves #1

--- ms-spot-the-difference/out.js
+++ ms-spot-the-difference/out.js
@@ -1,10 +1,24 @@
 const c = document.getElementById('spotdiff');
 
 let cycle;
-
 let cnum = 0;
 
-const colour_match = (a, b) => ((Math.abs(a[0] - b[0]) < 4) && (Math.abs(a[1] - b[1]) < 4) && (Math.abs(a[2] - b[2]) < 4));
+const colour_match = (a, b) => a[0] === b[0] && a[1] === b[1] && a[2] === b[2];
+
+const get_pixel = (idata, x, y, w) => {
+  let pos = y * w + x;
+  pos = pos * 4;
+  return [idata[pos], idata[pos + 1], idata[pos + 2], idata[pos + 3]];
+};
+
+const set_pixel = (idata, rgb, x, y, w) => {
+  let pos = y * w + x;
+  pos = pos * 4;
+  idata[pos] = rgb[0];
+  idata[pos + 1] = rgb[1];
+  idata[pos + 2] = rgb[2];
+  idata[pos + 3] = 255;
+}
 
 const spot_diff = async (blob) => {
   while (c.firstChild) c.removeChild(c.firstChild);
@@ -25,7 +39,8 @@ const spot_diff = async (blob) => {
   canvas.height = img.height;
   canvas.getContext("2d").drawImage(img, 0, 0);
 
-  const image = canvas.getContext("2d", { willReadFrequently: true });
+  const image = canvas.getContext("2d");
+  const image_d = image.getImageData(0, 0, canvas.width, canvas.height).data;
   
   const subimg = new Image();
 
@@ -41,7 +56,8 @@ const spot_diff = async (blob) => {
   subcanvas.height = subimg.height;
   subcanvas.getContext("2d").drawImage(subimg, 0, 0);
 
-  const image2 = subcanvas.getContext("2d", { willReadFrequently: true });
+  const image2 = subcanvas.getContext("2d");
+  const image2_d = image2.getImageData(0, 0, subcanvas.width, subcanvas.height).data;
   
   let streak = 0;
   let done = false;
@@ -52,10 +68,10 @@ const spot_diff = async (blob) => {
     streak = 0;
 
     for (let x = 0; x <= (canvas.width - subcanvas.width); x++) {
-      const canvas_pixel = image.getImageData(x, y, 1, 1);
-      const subcanvas_pixel = image2.getImageData(streak, 0, 1, 1);
+      const canvas_pixel = get_pixel(image_d, x, y, canvas.width);
+      const subcanvas_pixel = get_pixel(image2_d, streak, 0, subcanvas.width);
 
-      if (colour_match(canvas_pixel.data, subcanvas_pixel.data)) {
+      if (colour_match(canvas_pixel, subcanvas_pixel)) {
         streak++;
       } else {
         streak = 0;
@@ -84,85 +100,83 @@ const spot_diff = async (blob) => {
 
   const left_canvas = document.createElement("canvas");
   const right_canvas = document.createElement("canvas");
+  const diff_canvas_b = document.createElement("canvas");
+  const diff_canvas = document.createElement("canvas");
+  const diff_canvas2 = document.createElement("canvas");
 
   left_canvas.width = diff_width;
   left_canvas.height = diff_height;
-
   right_canvas.width = diff_width;
   right_canvas.height = diff_height;
-
-  left_canvas.getContext("2d").putImageData(left_img_data, 0, 0);
-  right_canvas.getContext("2d").putImageData(right_img_data, 0, 0);
-
-  c.appendChild(left_canvas);
-
-  const left_ctx = left_canvas.getContext("2d", { willReadFrequently: true });
-  const right_ctx = right_canvas.getContext("2d", { willReadFrequently: true });
-
-  const diff_canvas_b = document.createElement("canvas");
   diff_canvas_b.width = diff_width;
   diff_canvas_b.height = diff_height;
-
-  const diff_canvas = document.createElement("canvas");
   diff_canvas.width = diff_width;
   diff_canvas.height = diff_height;
-
-  const diff_canvas2 = document.createElement("canvas");
   diff_canvas2.width = diff_width;
   diff_canvas2.height = diff_height;
 
+  const left_ctx = left_canvas.getContext("2d");
+  const right_ctx = right_canvas.getContext("2d");
   const diff_b_ctx = diff_canvas_b.getContext("2d");
   const diff_ctx = diff_canvas.getContext("2d");
   const diff_ctx2 = diff_canvas2.getContext("2d");
 
-  diff_ctx.putImageData(left_ctx.getImageData(0, 0, diff_width, diff_height), 0, 0);
-  diff_ctx2.putImageData(left_ctx.getImageData(0, 0, diff_width, diff_height), 0, 0);
+  right_ctx.putImageData(right_img_data, 0, 0);
+  left_ctx.putImageData(left_img_data, 0, 0);
+  diff_ctx.putImageData(left_img_data, 0, 0);
+  diff_ctx2.putImageData(left_img_data, 0, 0);
 
   diff_b_ctx.fillStyle = 'black';
   diff_b_ctx.fillRect(0, 0, diff_width, diff_height);
 
+  const r_data = left_ctx.getImageData(0, 0, left_canvas.width, left_canvas.height).data;
+  const g_data = diff_ctx.getImageData(0, 0, left_canvas.width, left_canvas.height).data;
+  const b_data = diff_ctx2.getImageData(0, 0, left_canvas.width, left_canvas.height).data;
+  const a_data = diff_b_ctx.getImageData(0, 0, left_canvas.width, left_canvas.height).data;
+
   for (let y = 0; y < diff_height; y++) {
     for (let x = 0; x < diff_width; x++) {
-      const left_px = left_ctx.getImageData(x, y, 1, 1);
-      const right_px = right_ctx.getImageData(x, y, 1, 1);
+      const left_px = get_pixel(left_img_data.data, x, y, left_canvas.width);
+      const right_px = get_pixel(right_img_data.data, x, y, right_canvas.width);
 
-      if (!colour_match(left_px.data, right_px.data)) {
-        const r = new ImageData(new Uint8ClampedArray([255, 0, 0, 255]), left_px.width, left_px.height);
-        const g = new ImageData(new Uint8ClampedArray([0, 255, 0, 255]), left_px.width, left_px.height);
-        const b = new ImageData(new Uint8ClampedArray([0, 0, 255, 255]), left_px.width, left_px.height);
-        
-        left_ctx.putImageData(r, x, y);
-        diff_ctx.putImageData(g, x, y);
-        diff_ctx2.putImageData(b, x, y);
-        diff_b_ctx.putImageData(right_px, x, y);
+      if (!colour_match(left_px, right_px)) {
+        set_pixel(r_data, [255, 0, 0], x, y, left_canvas.width);
+        set_pixel(g_data, [0, 255, 0], x, y, left_canvas.width);
+        set_pixel(b_data, [0, 0, 255], x, y, left_canvas.width);
+        set_pixel(a_data, right_px, x, y, left_canvas.width);
       }
     }
   }
+
+  left_ctx.putImageData(new ImageData(r_data, left_canvas.width, left_canvas.height), 0, 0);
+  diff_ctx.putImageData(new ImageData(g_data, left_canvas.width, left_canvas.height), 0, 0);
+  diff_ctx2.putImageData(new ImageData(b_data, left_canvas.width, left_canvas.height), 0, 0);
+  diff_b_ctx.putImageData(new ImageData(a_data, left_canvas.width, left_canvas.height), 0, 0);
 
   left_canvas.style.display = 'inline';
   diff_canvas.style.display = 'none';
   diff_canvas2.style.display = 'none';
 
+  c.appendChild(left_canvas);
   c.appendChild(diff_canvas);
   c.appendChild(diff_canvas2);
   c.appendChild(diff_canvas_b);
 
   cycle = setInterval(() => {
     cnum = (cnum + 1) % 3;
+
+    left_canvas.style.display = 'none';
+    diff_canvas.style.display = 'none';
+    diff_canvas2.style.display = 'none';
+
     switch (cnum) {
       case 0:
         left_canvas.style.display = 'inline';
-        diff_canvas.style.display = 'none';
-        diff_canvas2.style.display = 'none';
         break;
       case 1:
-        left_canvas.style.display = 'none';
         diff_canvas.style.display = 'inline';
-        diff_canvas2.style.display = 'none';
         break;
       case 2:
-        left_canvas.style.display = 'none';
-        diff_canvas.style.display = 'none';
         diff_canvas2.style.display = 'inline';
         break;
     }
